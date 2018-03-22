ff61a98c3690fcab9fe44160fe828cabfe566767
yashsartanpara <yashsartanpara@gmail.com>
2018-03-22 12:41:19 +0530

    Update UI

--- js/index.js
+++ js/index.js
@@ -8,92 +8,93 @@ let owlCarouselTemplate = $("#owl-carousel-template").html();
 let compiledTemplate = Handlebars.compile(owlCarouselTemplate);
 
 const setData = (votes) => {
-  chartData = {
-    chart: votes
-  };
+    chartData = {
+        chart: votes
+    };
 
-  let generatedHTML = compiledTemplate(chartData);
-  $("#owl-carousel-container").html(generatedHTML);
+    let generatedHTML = compiledTemplate(chartData);
+    $("#owl-carousel-container").html(generatedHTML);
 
-  charts = chartData.chart.map((data, index) => {
-    return {
-      el: $("#cat" + index),
-      data: data.votes
-    }
-  });
-
-  pieCharts = charts.map(function (e) {
-    return new Chart(e.el, {
-      type: 'doughnut',
-      data: {
-        datasets: [{
-          label: "# of votes",
-          data: e.data,
-          backgroundColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
-          ]
-        }],
-        labels: [
-          "participant1",
-          "participant2",
-          "participant3",
-          "participant4",
-        ]
-      },
-      options: {
-        responsive: true,
-        responsiveAnimationDuration: 200,
-        maintainAspectRatio: true,
-        animation: {
-          animateRotate: true
+    charts = chartData.chart.map((data, index) => {
+        return {
+            el: $("#cat" + index),
+            data: data.votes
         }
-      }
     });
-  });
 
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: false
-  });
+    pieCharts = charts.map(function (e) {
+        return new Chart(e.el, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    label: "# of votes",
+                    data: e.data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ]
+                }],
+                labels: [
+                    "Participant1",
+                    "Participant2",
+                    "Participant3",
+                    "Participant4",
+                ]
+            },
+            options: {
+                responsive: true,
+                tooltips: false,
+                responsiveAnimationDuration: 0,
+                maintainAspectRatio: true,
+                animation: {
+                    animateRotate: false
+                }
+            }
+        });
+    });
+
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false
+    });
 
 };
 
 socket.on('init', (votes) => {
-  console.log(votes);
-  setData(votes);
+    console.log(votes);
+    setData(votes);
 });
 
 socket.on('vote', (votes) => {
-  updateData(pieCharts, votes);
+    updateData(pieCharts, votes);
 });
 
 
 const updateData = (pieCharts, votes) => {
-  chartData = {
-    chart: votes
-  };
+    chartData = {
+        chart: votes
+    };
 
-  charts = chartData.chart.map((data, index) => {
-    return {
-      el: $("#cat" + index),
-      data: data.votes
-    }
-  });
+    charts = chartData.chart.map((data, index) => {
+        return {
+            el: $("#cat" + index),
+            data: data.votes
+        }
+    });
 
-  for(let i=0; i<pieCharts.length;i++){
-    for(let j=0; j< pieCharts[i].data.datasets.length; j++){
-      pieCharts[i].data.datasets[j].data = charts[j].data;
-      console.log(pieCharts[i].data.datasets[j].data + "=" + charts[j].data)
+    for (let i = 0; i < pieCharts.length; i++) {
+        for (let j = 0; j < pieCharts[i].data.datasets.length; j++) {
+            pieCharts[i].data.datasets[j].data = charts[j].data;
+            console.log(pieCharts[i].data.datasets[j].data + "=" + charts[j].data)
+        }
     }
-  }
 };
