93554ee12a7c3b1588eee88b8e01e34c8100f634
yashsartanpara <yashsartanpara@gmail.com>
2018-03-25 00:32:02 +0530

    Add categories.js

---
+++ js/categories.js
@@ -0,0 +1,264 @@
+var data = {
+    "categories": [
+        {
+            "_id": "5ab10943cea18e00140dc812",
+            "nominees": [
+                {
+                    "_id": "5ab11077cea18e00140dc83b",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11077cea18e00140dc83c",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11077cea18e00140dc83d",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11077cea18e00140dc83e",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Student of the year"
+        },
+        {
+            "_id": "5ab1091dcea18e00140dc811",
+            "nominees": [
+                {
+                    "_id": "5ab1106acea18e00140dc837",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1106acea18e00140dc838",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1106acea18e00140dc839",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1106acea18e00140dc83a",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Artist of the year - Female"
+        },
+        {
+            "_id": "5ab108fecea18e00140dc80f",
+            "nominees": [
+                {
+                    "_id": "5ab11055cea18e00140dc82f",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11055cea18e00140dc830",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11055cea18e00140dc831",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11055cea18e00140dc832",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Style Icon"
+        },
+        {
+            "_id": "5ab108f6cea18e00140dc80e",
+            "nominees": [
+                {
+                    "_id": "5ab1104bcea18e00140dc82b",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1104bcea18e00140dc82c",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1104bcea18e00140dc82d",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1104bcea18e00140dc82e",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Rising Star"
+        },
+        {
+            "_id": "5ab1089acea18e00140dc80d",
+            "nominees": [
+                {
+                    "_id": "5ab11047cea18e00140dc827",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11047cea18e00140dc828",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11047cea18e00140dc829",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11047cea18e00140dc82a",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Face of the year - female"
+        },
+        {
+            "_id": "5ab10803cea18e00140dc809",
+            "nominees": [
+                {
+                    "_id": "5ab10f78cea18e00140dc813",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10f78cea18e00140dc814",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10f78cea18e00140dc815",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10f78cea18e00140dc816",
+                    "name": "Siddarth Goswami",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Sports Icon"
+        },
+        {
+            "_id": "5ab10918cea18e00140dc810",
+            "nominees": [
+                {
+                    "_id": "5ab11062cea18e00140dc833",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11062cea18e00140dc834",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11062cea18e00140dc835",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab11062cea18e00140dc836",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Artist of the year - Male"
+        },
+        {
+            "_id": "5ab10847cea18e00140dc80a",
+            "nominees": [
+                {
+                    "_id": "5ab10fcccea18e00140dc817",
+                    "name": "Darshan Dalal",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10fcccea18e00140dc818",
+                    "name": "Tushar Gonawala",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10fcccea18e00140dc819",
+                    "name": "Vatsal Trivedi",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10fcccea18e00140dc81a",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Face of the year"
+        },
+        {
+            "_id": "5ab10874cea18e00140dc80b",
+            "nominees": [
+                {
+                    "_id": "5ab10ffccea18e00140dc81b",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10ffccea18e00140dc81c",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10ffccea18e00140dc81d",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab10ffccea18e00140dc81e",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Mr. Persona"
+        },
+        {
+            "_id": "5ab1088ccea18e00140dc80c",
+            "nominees": [
+                {
+                    "_id": "5ab1102fcea18e00140dc823",
+                    "name": "Chintan Acharya",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1102fcea18e00140dc824",
+                    "name": "Pranshu Dave",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1102fcea18e00140dc825",
+                    "name": "Megharth Lakhataria",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                },
+                {
+                    "_id": "5ab1102fcea18e00140dc826",
+                    "name": "Yash Sartanpara",
+                    "imgUrl": "https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_boss_client_beard_male_person_user-512.png"
+                }
+            ],
+            "title": "Ms. Persona"
+        }
+    ]
+};
